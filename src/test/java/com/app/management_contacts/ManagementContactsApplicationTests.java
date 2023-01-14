ee37f5389d9e98f9d84e3eea418dc59f02e3f934
DuyLe <duy.le04@futa.vn>
2023-01-14 07:21:33 +0700

    initial commit

---
+++ src/test/java/com/app/management_contacts/ManagementContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.management_contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
